59ebff2d65cc45588b381f28da07bbf0caa1f620
rosancoderian <rosancoderian@gmail.com>
2023-02-06 20:44:23 +0700

    feat(web): add Menu component

---
+++ apps/web/src/lib/components/Menu/Menu.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  // TODO typing
+  export let menus: any[] = []
+</script>
+
+<ul class="menu bg-base-100 w-56">
+  {#each menus as menu}
+    <li class={$page.url.pathname === menu.url ? 'bordered' : ''}>
+      <a href={menu?.url}>{menu?.label}</a>
+    </li>
+  {/each}
+</ul>
