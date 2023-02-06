aa348051ff347a790c9b3bb1bfc34cefe06d42b2
rosancoderian <rosancoderian@gmail.com>
2023-02-06 07:50:57 +0700

    feat(web): no account verify check

--- apps/web/src/routes/login/+page.server.ts
+++ apps/web/src/routes/login/+page.server.ts
@@ -8,10 +8,6 @@ export const actions = {
       await locals.pb
         .collection('users')
         .authWithPassword(body.email, body.password)
-      if (!locals.pb.authStore.model.verified) {
-        locals.pb.authStore.clear()
-        return fail(401, { notVerified: true })
-      }
     } catch (err: any) {
       console.error('Error: ', err)
       return fail(err.status, { errors: err.data.data })
