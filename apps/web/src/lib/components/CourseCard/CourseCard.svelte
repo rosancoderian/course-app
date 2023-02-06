e65139840931b7c88420aec3bc47e6dfaf2e02ea
rosancoderian <rosancoderian@gmail.com>
2023-02-06 23:42:29 +0700

    feat(web): add EnrollModal component

--- apps/web/src/lib/components/CourseCard/CourseCard.svelte
+++ apps/web/src/lib/components/CourseCard/CourseCard.svelte
@@ -1,5 +1,6 @@
 <script type="ts">
   import { PUBLIC_PB_BASE_URL } from '$env/static/public'
+  import EnrollModal from '../EnrollModal/EnrollModal.svelte'
 
   export let id = ''
   export let title = ''
@@ -7,15 +8,20 @@
   export let desc = ''
 
   const imageUrl = `${PUBLIC_PB_BASE_URL}/api/files/courses/${id}/${image}`
+  const courseUrl = `course/${id}`
+  const enrollModalId = `enroll-modal-${id}`
 </script>
 
-<div class="card w-96 h-96 bg-base-100 border border-base-300 shadow-xl">
-  <figure><img class="object-cover" src={imageUrl} alt={title} /></figure>
+<div class="card min-h-96 bg-base-100 border border-base-300 shadow-xl">
+  <figure>
+    <img class="object-cover w-[320px] h-[240px]" src={imageUrl} alt={title} />
+  </figure>
   <div class="card-body">
-    <h2 class="card-title">{title}</h2>
+    <h2 class="card-title link"><a href={courseUrl}>{title}</a></h2>
     <p>{desc}</p>
     <div class="card-actions">
-      <button class="btn btn-primary w-full">Enroll</button>
+      <label for={enrollModalId} class="btn btn-primary w-full"> Enroll </label>
+      <EnrollModal id={enrollModalId} title={`Enroll to ${title}`} />
     </div>
   </div>
 </div>
---
+++ apps/web/src/lib/components/EnrollModal/EnrollModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let id: string
+  export let title: string
+</script>
+
+<input {id} type="checkbox" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <label for={id} class="btn btn-sm btn-circle absolute right-2 top-2"
+      >✕</label
+    >
+    <h1 class="py-4 font-bold">
+      {title}
+    </h1>
+    Are you sure want to enroll to this course?
+    <div class="modal-action">
+      <label for={id} class="btn btn-primary">Yes</label>
+    </div>
+  </div>
+</div>
