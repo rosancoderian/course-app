2c0571725005d57af95ff807a9b8abe33d47de3e
rosancoderian <rosancoderian@gmail.com>
2023-02-06 07:46:37 +0700

    fix(web): fix non-POJO error

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,9 +1,10 @@
+import toPOJO from '$lib/utils/toPOJO'
 import type { ServerLoad } from '@sveltejs/kit'
 
 export const load: ServerLoad = ({ locals }) => {
   if (locals.user) {
     return {
-      user: locals.user,
+      user: toPOJO(locals.user),
     }
   }
 
--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import pb from '$lib/pocketbase/pb'
+import toPOJO from '$lib/utils/toPOJO'
 
 export async function load() {
   const coursesData = await pb.collection('courses').getFullList()
   return {
-    courses: JSON.parse(JSON.stringify(coursesData)),
+    courses: toPOJO(coursesData),
   }
 }
