b2b04f454fd832a13a247a00502b0ef6bd75db0b
rosancoderian <rosancoderian@gmail.com>
2023-02-06 07:17:56 +0700

    feat(web): add AccountLayout

---
+++ apps/web/src/lib/layouts/AccountLayout/AccountLayout.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col item-center h-full w-full">
+  <h2 class="mt-2 text-center text-3x font-bold">
+    <slot name="title" />
+  </h2>
+  <p class="text-center mt-1">
+    <slot name="subTitle" />
+  </p>
+  <slot/>
+</div>
--- apps/web/src/routes/register/+page.svelte
+++ apps/web/src/routes/register/+page.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
   import { applyAction, enhance } from '$app/forms'
   import Input from '$lib/components/Input/Input.svelte'
+  import AccountLayout from '$lib/layouts/AccountLayout/AccountLayout.svelte'
   import getFormError from '$lib/utils/getFormError'
 
   let errors: Record<string, any> = {}
   let isLoading = false
 </script>
 
-<div class="flex flex-col item-center h-full w-full">
-  <h2 class="mt-2 text-center text-3x font-bold">Register Account</h2>
-  <p class="text-center mt-1">
+<AccountLayout>
+  <span slot="title">Register</span>
+  <span slot="subTitle">
     Or <a
       href="/login"
-      class="text-primary font-medium hover:cursor-pointer hover:underline"
+      class="link"
       >Login
     </a> with existing account
-  </p>
+  </span>
+
   <form
     use:enhance={({ form, data, action, cancel }) => {
       isLoading = true
@@ -33,10 +35,30 @@
     class="flex flex-col items-center space-y-2 w-full pt-4"
   >
     <div class="form-control w-full max-w-md">
-      <Input type="text" label="Username" name="username" error={getFormError(errors, 'username')} />
-      <Input type="text" label="Email" name="email" error={getFormError(errors, 'email')} />
-      <Input type="password" label="Password" name="password" error={getFormError(errors, 'password')} />
-      <Input type="password" label="Password Confirm" name="passwordConfirm" error={getFormError(errors, 'passwordConfirm')} />
+      <Input
+        type="text"
+        label="Username"
+        name="username"
+        error={getFormError(errors, 'username')}
+      />
+      <Input
+        type="text"
+        label="Email"
+        name="email"
+        error={getFormError(errors, 'email')}
+      />
+      <Input
+        type="password"
+        label="Password"
+        name="password"
+        error={getFormError(errors, 'password')}
+      />
+      <Input
+        type="password"
+        label="Password Confirm"
+        name="passwordConfirm"
+        error={getFormError(errors, 'passwordConfirm')}
+      />
       <div class="pt-8">
         <button
           type="submit"
@@ -46,4 +68,4 @@
       </div>
     </div>
   </form>
-</div>
+</AccountLayout>
