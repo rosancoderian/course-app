af8709a4daf48c84477435ce5f74e0e70d4f9ad2
rosancoderian <rosancoderian@gmail.com>
2023-02-08 00:30:46 +0700

    fix(web): fix admin redirection

--- apps/web/src/routes/(dashboard)/_/+layout.server.ts
+++ apps/web/src/routes/(dashboard)/_/+layout.server.ts
@@ -1,9 +1,8 @@
 import isAdmin from '$lib/utils/isAdmin'
 import { redirect, type RequestEvent } from '@sveltejs/kit'
-import { Admin } from 'pocketbase'
 
 export const load = async ({ locals }: RequestEvent) => {
-  if (!locals.pb.authStore.isValid || isAdmin(locals.user)) {
+  if (!locals.pb.authStore.isValid || !isAdmin(locals.user)) {
     throw redirect(303, '/')
   }
 }
