6728be4df8f091f8b30487591367bf7303fcc7bb
rosancoderian <rosancoderian@gmail.com>
2023-02-07 21:56:09 +0700

    feat(web): add COurseGridLayout

---
+++ apps/web/src/lib/layouts/CourseGridLayout/CourseGridLayout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import classNames from 'classnames'
+
+  export let maxCol: string | number = 5
+</script>
+
+<div
+  class={classNames([
+    'grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4',
+    `2xl:grid-cols-${maxCol}`,
+    'justify-items-stretch gap-4',
+  ])}>
+  <slot />
+</div>
