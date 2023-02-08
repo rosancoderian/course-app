f98900d417b4392e21f4b62f5249e9a01675aeb8
rosancoderian <rosancoderian@gmail.com>
2023-02-09 04:12:53 +0700

    feat(web): add FileInput Component

---
+++ apps/web/src/lib/components/TextArea/TextArea.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Field from '../Field/Field.svelte'
+
+  export let label: string
+  export let name: string
+  export let error: string = ''
+  export let value: string = ''
+</script>
+
+<Field {name} {label} {error}>
+  <textarea name="desc" class="textarea textarea-bordered h-24 resize-none"
+    >{value}</textarea>
+</Field>
