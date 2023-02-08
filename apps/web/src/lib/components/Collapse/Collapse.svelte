4292982dbeb6f833f1832a7cc5f48f628ef7af10
rosancoderian <rosancoderian@gmail.com>
2023-02-09 04:17:20 +0700

    add(web): add Collapse component. TODO: make it works properly

---
+++ apps/web/src/lib/components/Collapse/Collapse.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import classNames from 'classnames'
+
+  export let tabindex: number = 0
+  export let title: string = ''
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<div
+  {tabindex}
+  class={classNames([
+    'collapse collapse-arrow border border-base-300 bg-base-100 rounded-box',
+  ])}>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class="collapse-title text-md font-medium hover:cursor-pointer">
+    {title}
+  </div>
+  <div class="collapse-content">
+    <slot />
+  </div>
+</div>
